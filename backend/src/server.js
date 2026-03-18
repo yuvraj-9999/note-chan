4f74701dd762354d4ec57e46df26f7312e5f99c2
Yuvraj Sabale <yuvrajsabale999@gmail.com>
2026-03-18 14:05:56 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,22 @@ import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js"
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
 
+const __dirname = path.resolve();
 
 
-// middleware 
+
+// middleware for development
+if (process.env.NODE_ENV !== "production") {
+
+    app.use(cors());
+}
 app.use(cors());
 app.use(express.json()); //this middleware will parse JSON bodies: req.body
 
@@ -21,6 +28,14 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+// serve our optimized react application as a static asset 
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 connectDB().then(() => {
 
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// in production there is no localhost it will be dynanmic so we have to make our baseURL dynamic 
+const BASE_URL = import.meta.env.MODE = "development" ? "ttp://localhost:5001/api" : "/api";
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASE_URL,
 });
 
 export default api;
