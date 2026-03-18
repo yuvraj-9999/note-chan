74d3f50129dda477ca235f528ea67bd2c1e69e01
Yuvraj Sabale <yuvrajsabale999@gmail.com>
2026-03-18 14:25:34 +0530

    Fixed some of the deployment issues

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // in production there is no localhost it will be dynanmic so we have to make our baseURL dynamic 
-const BASE_URL = import.meta.env.MODE = "development" ? "ttp://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
     baseURL: BASE_URL,
 });
